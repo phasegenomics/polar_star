2bf27a36342dc96cfc31e55e76bb25fe36487e5e
EC2 Default User <ec2-user@ip-172-31-11-6.us-west-2.compute.internal>
2017-11-22 04:45:21 +0000

    up and running alignments

--- README.md
+++ README.md
@@ -1,1 +1,3 @@
 # polar_star
+
+This code is a snakemake pipeline for breaking contigs based on long-read depth
--- setup/env_setup.sh
+++ setup/env_setup.sh
@@ -1,7 +1,12 @@
-
-sudo pip install --upgrade pip
+sudo yum -y install git 
 sudo yum -y install python36
+sudo yum -y install go
+sudo yum -y install zlib-devel
+sudo yum -y install ncurses-devel
+sudo yum -y install autoconf
+sudo yum -y install bzip2-devel-1.0.6-8.12.amzn1.x86_64
+sudo yum -y install xz-devel-5.1.2-12alpha.12.amzn1.x86_64
 
-virtualenv -p python36 ~/tmp
-source ~/tmp/bin/activate
+virtualenv -p python36 ~/py_36_env
+source ~/py_36_env/bin/activate
 pip install snakemake
