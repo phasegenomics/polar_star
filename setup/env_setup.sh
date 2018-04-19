c2d2a54f371860eac90db1ff8637d9d59e465d15
Zev Kronenberg <zev.kronenberg@gmail.com>
2018-04-19 13:52:43 -0700

    Update env_setup.sh

--- setup/env_setup.sh
+++ setup/env_setup.sh
@@ -12,7 +12,8 @@ sudo yum -y install xz-devel-5.1.2-12alpha.12.amzn1.x86_64
 sudo yum -y install python36-devel
 sudo python3.6 -m pip install --upgrade pip
 sudo python3.6 -m pip install --upgrade virtualenvwrapper
+sudo python3.6 -m pip install --upgrade snakemake
+
 
 virtualenv -p python36 ~/py_36_env
 source ~/py_36_env/bin/activate
-sudo pip-3.6 install snakemake
